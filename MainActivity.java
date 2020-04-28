3818bfc6bc27f1830c723f2597e3987e6fd5abdd
Satyajit Pany <64101421+satyajitpany@users.noreply.github.com>
2020-04-28 16:51:20 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,67 @@
+package com.cromptex.odishatelevisoin;
+
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+        import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new myWebClient());
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://odishatelevision.com/");
+
+    webView.getSettings().setGeolocationEnabled(true);
+    webView.setWebViewClient(new WebViewClient()
+    {
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error)
+        {
+            handler.proceed(); // Ignore SSL certificate errors
+        }
+    });
+
+    }
+
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            view.loadUrl(url);
+            return true;
+
+        }
+    }
+
+    @Override
+    // This method is used to detect back button
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // Let the system handle the back button
+            super.onBackPressed();
+        }
+    }
+}
